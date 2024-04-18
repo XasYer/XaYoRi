612de8f34d995800df8ab75407a2f98a52244afd
caimingtr <1564571378@qq.com>
2024-04-19 03:14:19 +0800

    fix: result.id is defined

--- index.js
+++ index.js
@@ -115,9 +115,7 @@ class SatoriBot {
             channel_id: group_id,
             content
         })).pop()
-        return {
-            message_id: `${group_id}-${result.id}`
-        }
+        return result ? { message_id: `private:${user_id}-${result.id}`} : logger.error('发送群聊消息失败: 暂不支持此类型消息')
     }
 
     async sendPrivateMsg(user_id, msg) {
@@ -127,9 +125,7 @@ class SatoriBot {
             channel_id: `private:${user_id}`,
             content
         })).pop()
-        return {
-            message_id: `private:${user_id}-${result.id}`
-        }
+        return result ? { message_id: `private:${user_id}-${result.id}`} : logger.error('发送好友消息失败: 暂不支持此类型消息')
     }
 
     /**
@@ -497,7 +493,7 @@ class SatoriBot {
 
     async getImageMD5(url) {
         const buffer = Buffer.from(await (await fetch(url)).arrayBuffer())
-        const hash = createHash('md5');
+        const hash = createHash('md5')
         hash.update(buffer);
         return hash.digest('hex').toUpperCase()
     }
